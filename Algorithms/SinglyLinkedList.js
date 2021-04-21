d68944f396b91f2b891dea9b1958e3322ecf1f16
AL-SAFWAN <al-safwan.uddin@abehr.com>
2021-04-21 16:12:15 +0100

    SignlyLinkedList

---
+++ Algorithms/SinglyLinkedList.js
@@ -0,0 +1,143 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.length = 0;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  pop() {
+    if (!this.head) return undefined;
+
+    var current = this.head;
+    let newTail = current;
+    while (current) {
+      if (!current.next) {
+        this.tail = newTail;
+        this.tail.next = null;
+        this.length--;
+      }
+      if (this.length === 0) {
+        this.head = null;
+        this.tail = null;
+      }
+      newTail = current;
+      current = current.next;
+    }
+  }
+  shift() {
+    if (!this.head) return undefined;
+    let currentHead = this.head;
+    this.head = currentHead.next;
+    this.length--;
+    if (this.length === 0) {
+      this.tail = null;
+    }
+    return currentHead;
+  }
+  traverse() {
+    var current = this.head;
+    while (current) {
+      console.log(current.val);
+      current = current.next;
+    }
+  }
+  unshift(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  get(index) {
+    if (index < 0 || index >= this.length) return undefined;
+    var current = this.head;
+    let i = 0;
+    while (i !== index) {
+      i++;
+      current = current.next;
+    }
+    return current;
+  }
+  set(index, value) {
+    let node = this.get(index);
+    if (node) {
+      node.val = value;
+      return true;
+    }
+    return false;
+  }
+  insert(index, value) {
+    if (index < 0 || index > this.length) return false;
+    if (index === this.length) return !!this.push(value);
+    if (index === 0) return !!this.unshift(value);
+
+    var newNode = new Node(value);
+    var prev = this.get(index - 1);
+
+    var temp = prev.next;
+    prev.next = newNode;
+    newNode.next = temp;
+
+    this.length++;
+    return true;
+  }
+  remove(index) {
+    if (index < 0 || index >= this.length) return false;
+    if (index === this.length - 1) return !!this.pop();
+    if (index === 0) return !!this.shift();
+
+    var previousNode = this.get(index - 1);
+    var removed = previousNode.next;
+    previousNode.next = removed.next;
+    this.length--;
+    return removed;
+  }
+  reverse() {
+    var node = this.head;
+    this.head = this.tail;
+    this.tail = node;
+    var next;
+    var prev = null;
+    for (var i = 0; i < this.length; i++) {
+      next = node.next;
+      node.next = prev;
+      prev = node;
+      node = next;
+    }
+  }
+}
+
+let list = new SinglyLinkedList();
+list.push("hello");
+list.push("world");
+list.push("1");
+list.push("2");
+list.push("3");
+list.reverse();
+list.pop();
+list.shift();
+list.unshift("awesome dude");
+list.traverse();
